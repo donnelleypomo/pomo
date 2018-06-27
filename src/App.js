3ce58006810661bca6ccfabb0b9bf0f5f377b4b5
ANACAPA\Administrator <tom@tomfollett.com>
2018-06-27 09:48:45 -0700

    pseudo-code 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.setState({focusPeriodDuration: 25});
+    // App state
+      // sessionNumber: 1,  //resetApp()
+     
+      // focusPeriodDuration: 25,
+      // restPeriodDuration: 5,
+      // breakPeriodDuration: 30;
+      // mode: SET_UP, // TIMER_RUNNING
+      // tasks
+
+      // callBack from start, start 1 sec timer
+  };
+
+  
   render() {
+    const seconds = 301;
+
+    const TimerDisplay = props => 
+      <div>
+        {Math.floor(props.seconds / 60)}
+        :
+        {props.seconds % 60}  
+      </div>;
+  
     return (
+
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <TimerDisplay seconds={seconds} />
       </div>
+    /*  if state.mode = "TIMER_RUNNING" 
+          <PomodoroPanel> reset app button  show session number 
+          timer F
+          timer R
+          </PomodoroPanel>
+
+          <TasksPanel> </TasksPanel>
+        if state.mode = "SET_UP"  
+        <StartupPanel> 
+           start button three durations save to state, save on enter, error check limits
+           demo mode buttn
+           callback on start
+        </StartupPanel>
+        */
     );
   }
 }
+
+/*
+  never both zero?
+
+  StartFocusPeriod()
+    activeTimer = "FOCUS"
+    focusTime = *
+    announce
+    sessions++
+  StartBreakPeriod()
+    activeTimer = "BREAK"
+    focusTime = 0
+    breakTime = * (take sessionbs into account)
+    announce
+
+  
+
+  init cycle
+    focusTimer = 0; breakTimer = 0; sessions = 0; start timer StartFocusPeriod()
+  cycle
+    if (activeTimer == "FOCUS") (focusTime == 0) ? StartBreakPeriod() : focusTime--
+    else (breakTime == 0) ? StartFocusPeriod() : breakTimer--
+  
+
+*/
 
 export default App;
